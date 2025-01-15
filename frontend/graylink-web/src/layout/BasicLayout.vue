2fd8dc477eb1a29fe995e7320c436de5610c04d5
graysui <131964332+graysui@users.noreply.github.com>
2025-01-15 11:25:19 +0800

    build bug

--- frontend/graylink-web/src/layout/BasicLayout.vue
+++ frontend/graylink-web/src/layout/BasicLayout.vue
@@ -2,40 +2,11 @@
   <el-container class="layout-container">
     <el-aside :width="isCollapse ? '64px' : '200px'" class="aside">
       <div class="logo">
-        <img src="@/assets/logo.png" alt="Logo">
-        <span v-if="!isCollapse">GrayLink</span>
+        <span v-show="!isCollapse">GrayLink</span>
       </div>
-      <el-menu
-        :collapse="isCollapse"
-        :default-active="activeMenu"
-        class="el-menu-vertical"
-        background-color="#304156"
-        text-color="#bfcbd9"
-        active-text-color="#409EFF"
-        router
-      >
-        <el-menu-item index="/monitor">
-          <el-icon><Monitor /></el-icon>
-          <template #title>监控管理</template>
-        </el-menu-item>
-        <el-menu-item index="/file">
-          <el-icon><FolderOpened /></el-icon>
-          <template #title>文件管理</template>
-        </el-menu-item>
-        <el-menu-item index="/symlink">
-          <el-icon><Connection /></el-icon>
-          <template #title>软链接管理</template>
-        </el-menu-item>
-        <el-menu-item index="/emby">
-          <el-icon><VideoCamera /></el-icon>
-          <template #title>Emby 管理</template>
-        </el-menu-item>
-        <el-menu-item index="/setting">
-          <el-icon><Tools /></el-icon>
-          <template #title>系统设置</template>
-        </el-menu-item>
-      </el-menu>
+      <Sidebar :is-collapse="isCollapse" />
     </el-aside>
+
     <el-container>
       <el-header class="header">
         <div class="header-left">
@@ -71,7 +42,7 @@ import { ref, computed } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import { FolderOpened, Connection, VideoCamera, Monitor, Tools, Expand, Fold } from '@element-plus/icons-vue'
 import Breadcrumb from './Breadcrumb.vue'
-import { useUserStore } from '@/stores/user'
+import { useUserStore } from '@/stores/modules/user'
 
 const isCollapse = ref(false)
 const route = useRoute()
@@ -105,14 +76,11 @@ const handleLogout = async () => {
   height: 60px;
   display: flex;
   align-items: center;
+  justify-content: center;
   padding: 0 16px;
   color: #fff;
-}
-
-.logo img {
-  width: 32px;
-  height: 32px;
-  margin-right: 12px;
+  font-size: 18px;
+  font-weight: bold;
 }
 
 .el-menu-vertical {
